94972bd15b9981e2132e0f65d2534977bf8aef4d
Mohamed Noufal <getmohamednoufal@gmail.com>
2022-03-21 16:38:35 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div name="home" className=" w-full h-screen bg-[#0a192f] ">
+            {/* container */}
+            <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full ">
+                <p>Hi, my name is 0</p>
+                <h1>Mohamed Noufal</h1>
+                <h2>I'm a Full stack developer.</h2>
+                <p>
+                    Am an aspiring web developer with an experience of 6 months as a full-stack developer intern, looking
+                    for a position to incorporate my skills and training to help the company grow. I am looking forward to
+                    roles that will help me understand my potential by exploring the various aspects of this field.{" "}
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
